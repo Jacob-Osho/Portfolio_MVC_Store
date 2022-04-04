6cb4b12ce842e2890478eecde7264671f6403e47
Jacob <akella2609@gmail.com>
2022-04-04 21:02:28 +0200

    bug fix

---
+++ MVC_Store/Models/ViewModels/Pages/PageVM.cs
@@ -0,0 +1,38 @@
+﻿using MVC_Store.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Store.Models.ViewModels.Pages
+{
+    public class PageVM
+    {
+        public PageVM()
+        {
+
+        }
+        public PageVM(PagesDTO pages)
+        {
+            Id = pages.Id;
+            Title = pages.Title;
+            Body = pages.Body;
+            Slug = pages.Slug;
+            Sorting = pages.Sorting;
+            HasSidebar = pages.HasSidebar;
+
+        }
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50,MinimumLength =3)]
+        public string Title { get; set; }
+        [Required]
+        [StringLength(int.MaxValue, MinimumLength = 3)]
+        public string Body { get; set; }
+        public string Slug { get; set; }
+        public int Sorting { get; set; }
+        [Display(Name ="SideBar")]
+        public bool HasSidebar { get; set; }
+    }
+}
