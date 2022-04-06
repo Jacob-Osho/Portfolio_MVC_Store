f0531631ada10d0085712da139cacc19921ebc1e
Jacob <akella2609@gmail.com>
2022-04-06 15:25:33 +0200

    added methods for adding deleating and sorting categories

--- MVC_Store/Areas/Admin/Controllers/ShopController.cs
+++ MVC_Store/Areas/Admin/Controllers/ShopController.cs
@@ -24,7 +24,7 @@ namespace MVC_Store.Areas.Admin.Controllers
                     .ToList();
             }
             // возвращаем лист с представлениями
-       
+
             return View(categoryVMList);
         }
         //Метод Удаления страниц
@@ -33,22 +33,75 @@ namespace MVC_Store.Areas.Admin.Controllers
         {
             using (Db db = new Db())
             {
-                //получаем данные страницы по айди
+                //получаем данные категории по айди
                 CategoryDTO dto = db.Categories.Find(id);
 
-                //удаляем  страницу
+                //удаляем  категорию
                 db.Categories.Remove(dto);
 
                 // сохраняем изменения в базе
                 db.SaveChanges();
             }
-            //переадресовываем пользователя  на страницу индекс
+          
 
             //Оповесщаем пользователя о упешном сохронении  
             TempData["SM"] = "You have succesfully deleted page";
-            return RedirectToAction("Index");
+            //переадресовываем пользователя  на страницу категории
+            return RedirectToAction("Categories");
         }
-       
-    
-    }
+      
+        //9
+        // POST: Admin/Shop/AddNewCategory
+        [HttpPost]
+        public string AddNewCategory(string catName)
+        {
+            //обьявляем строковую прееменную айди
+            string id;
+            using(Db db =new Db())
+            {
+                //провекра имя категории на уникальность
+                 if (db.Categories.Any(x => x.Name == catName))
+                 {
+                    return "titletaken";
+                 }
+                //инициализируем модель ДТО
+                    CategoryDTO dto = new CategoryDTO();
+                //заполняем данными модель
+                dto.Name = catName;
+                dto.Slag = catName.Replace(" ", "-").ToLower();
+                dto.Sorting = 100;
+                //сохраняем
+                db.Categories.Add(dto);
+                db.SaveChanges();
+                //получаем айди что бы вернуть его представление
+                id = dto.Id.ToString();
+            }
+            //Возвращаем айди 
+            return id;
+        }
+
+        //9
+        // Post: Admin/Shop/ReorderCategories
+        [HttpPost]
+        public void ReorderCategories(int[] id)
+        {
+            using (Db db = new Db())
+            {
+                //счетчик
+                int count = 1;//еденица потому что хоум мы не будем сортировать
+
+                //иннициализируем модель данных
+                CategoryDTO dto;
+
+                //устанавливаем сортировку для каждой страницы
+                foreach (var catId in id)
+                {
+                    dto = db.Categories.Find(catId);
+                    dto.Sorting = count;
+                    db.SaveChanges();
+                    count++;
+                }
+            }
+        }
+    } 
 }
