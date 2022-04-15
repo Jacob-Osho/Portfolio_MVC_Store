9be41fc905f0567de00699affcd2aa8d0562d549
Jacob <akella2609@gmail.com>
2022-04-15 17:46:46 +0200

    Finished with Role features started working on checkout

--- MVC_Store/Global.asax.cs
+++ MVC_Store/Global.asax.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using MVC_Store.Models.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -16,6 +18,41 @@ namespace MVC_Store
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+        //25
+        //метод обработки запросов аутентефикации
+        protected void Application_AuthenticateRequest()
+        {
+            // Проверяем авторизированн ли пользователь
+            if (User == null)
+                return;
+            // получаем имя ползователя
+            string userName = Context.User.Identity.Name;
+
+            // обьявляем массив ролей
+            string[] roles = null;
+
+           
+            using(Db db = new Db())
+            {
+                // заполняем роли
+                UserDTO dto =db.Users.FirstOrDefault(x =>x.UserName == userName);
+                if (dto == null)
+                    return;
+                else
+                {
+                    roles = db.UserRoles.Where(x => x.UserId == dto.Id)
+                                            .Select(x => x.Role.Name)
+                                            .ToArray();
+                }
+                    
+            }
+            // создаем обьект интерфейса Iprincipal
+            IIdentity userIdenity = new GenericIdentity(userName);
+            IPrincipal newUserObj = new GenericPrincipal(userIdenity, roles);
+
+            // обьявляем  и инициализируем  данными  Context.user
+            Context.User = newUserObj;
         }
     }
 }
